8ef935cf5fa6f9601eef1c8d3fbc21e39fe13971
Domenica Aron <daron@ox-s5.21-school.ru>
2019-09-11 15:25:22 +0300

    all

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daron <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/09 17:48:24 by daron             #+#    #+#             */
+/*   Updated: 2019/09/10 16:24:35 by daron            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include "libft.h"
+
+# define BUFF_SIZE 32
+
+int get_next_line(const int fd, char **line);
+
+#endif
