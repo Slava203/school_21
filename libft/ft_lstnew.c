fabe0fa8dddd3a00900f67a72ba5ef009c3da6f4
Domenica Aron <daron@ox-s1.21-school.ru>
2019-09-18 18:29:12 +0300

    ALL

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daron <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/05 10:51:18 by daron             #+#    #+#             */
+/*   Updated: 2019/09/10 12:43:09 by daron            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list *elem;
+
+	if (!(elem = (t_list*)malloc(sizeof(t_list))))
+		return (NULL);
+	if (content == NULL)
+	{
+		elem->content = NULL;
+		elem->content_size = 0;
+	}
+	else
+	{
+		if (content_size == 0 || !(elem->content = malloc(content_size)))
+			return (NULL);
+		ft_memcpy(elem->content, content, content_size);
+		elem->content_size = content_size;
+	}
+	elem->next = NULL;
+	return (elem);
+}
